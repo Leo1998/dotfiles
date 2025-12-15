8e1a4b20be98a8973a3378bb96359edf9cc1d648
Leo1998 <leonard.fricke98@gmail.com>
2025-12-15 20:37:37 +0100

    push

--- heaven/.screenlayout/default.sh
+++ heaven/.screenlayout/default.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-1 --off --output DP-2 --off --output HDMI-1 --off --output HDMI-2 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DVI-D-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI-1-3 --off --output DP-1-3 --off --output DP-1-4 --off --output DP-1-5 --off
+xrandr --output DisplayPort-0 --off --output DisplayPort-1 --off --output HDMI-A-0 --off --output HDMI-A-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DVI-D-0 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI-A-1-2 --off --output DisplayPort-1-2 --off --output DisplayPort-1-3 --off --output DisplayPort-1-4 --off
