c65eca639db13f4e03d4846369bf4496dadab5f7
Leo1998 <leonard.fricke98@gmail.com>
2025-12-28 16:07:42 +0100

    update

--- arch/install.sh
+++ arch/install.sh
@@ -7,8 +7,6 @@ if [ "$_distro" = "arch" ]; then
     sudo cp clean-remove.hook /etc/pacman.d/hooks/
     sudo cp clean-upgrade.hook /etc/pacman.d/hooks/
 
-    sudo cp reflector.conf /etc/xdg/reflector/
-    sudo systemctl enable --now reflector.timer
-
     sudo cp 50-keyboard.conf /etc/X11/xorg.conf.d/
+    sudo cp 20-amdgpu.conf /etc/X11/xorg.conf.d/
 fi
