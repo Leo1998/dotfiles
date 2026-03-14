a1d0f49ca0e64a1a561b016965d83b6cfa49cdd2
Leo1998 <leonard.fricke98@gmail.com>
2026-03-14 12:47:35 +0100

    cleanup

--- arch/install_hooks.sh
+++ arch/install_hooks.sh
@@ -6,7 +6,4 @@ if [ "$_distro" = "arch" ]; then
     sudo mkdir -p /etc/pacman.d/hooks
     sudo cp clean-remove.hook /etc/pacman.d/hooks/
     sudo cp clean-upgrade.hook /etc/pacman.d/hooks/
-
-    sudo cp 50-keyboard.conf /etc/X11/xorg.conf.d/
-    sudo cp 20-amdgpu.conf /etc/X11/xorg.conf.d/
 fi
